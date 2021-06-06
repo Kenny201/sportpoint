07ee40fda6eb3f84c7785c1d65b048538cc4ff48
Your Name <you@example.com>
2021-06-06 23:50:02 +0300

    Доделал попап

--- оформление заказа/script.js
+++ оформление заказа/script.js
@@ -13,28 +13,21 @@ $(document).on('click', '.bx-soa .licence_block label.license', function(){
 });
 
 $(document).ready(function() {
-  var show = false;
   $('.bx-soa-section-content').on('click','.step_next', function(e) {
       e.preventDefault();
       let current = $(this),
-      next = $(this).parents('.bx-soa-section').next('.bx-soa-section').find('.bx-soa-section-content');  
+      next = current.parents('.bx-soa-section').next('.bx-soa-section').find('.bx-soa-section-content');  
       let content = current.parents('.bx-soa-section-content');
       if ( next.find('.step_next').length || next.find('.step_prev').length){
-          show = false;
           content.slideUp(350, "linear");
           next.slideDown(350, "linear");
           next.find('.step_prev').show();
       }else {
-        if (show) {
-          content.slideUp(350, "linear");
-          next.slideDown(350, "linear");
-         
-        }
         content.slideUp(350, "linear");
         next.slideDown(350, "linear");
         next.find('.step_wrap').prepend('<div class="step_next"> <button class="btn btn-default"> Далее </button></div></div>')
         next.find('.step_wrap').prepend('<div class="step_prev"> <button class="btn btn-default"> Назад </button></div></div>')
-        if ($(this).parents('.bx-soa-section').next('.bx-soa-section').attr('id') == 'bx-soa-basket'){
+        if (current.parents('.bx-soa-section').next('.bx-soa-section').attr('id') == 'bx-soa-basket'){
           next.find('.step_prev').show();
           next.find('.step_next').hide();
         }else{
--- оформление заказа/Оформление заказа - SportPoint_files/default_88e11775faaaabbd255fdfb9d9f13887_v1.js
+++
@@ -1,9 +0,0 @@
-
-; /* Start:"a:4:{s:4:"full";s:90:"/local/templates/aspro_max/components/bitrix/subscribe.edit/footer/script.js?1618401024113";s:6:"source";s:76:"/local/templates/aspro_max/components/bitrix/subscribe.edit/footer/script.js";s:3:"min";s:0:"";s:3:"map";s:0:"";}"*/
-$(document).ready(function(){
-	$("form.subscribe-form").validate({
-		rules:{ "EMAIL": {email: true} }
-	});
-})
-/* End */
-;; /* /local/templates/aspro_max/components/bitrix/subscribe.edit/footer/script.js?1618401024113*/
